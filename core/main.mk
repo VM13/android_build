5f49d24893c34ef1cfdd4c359e1530f589c4d119
cryptickid <cryptickid@gmail.com>
2015-12-12 15:59:40 -0500

    build it vm13

--- core/main.mk
+++ core/main.mk
@@ -183,9 +183,7 @@ endif
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
 requires_openjdk := false
 ifeq ($(HOST_OS), linux)
-ifeq ($(ALLOW_SE_JAVA7),)
 requires_openjdk := true
-endif
 endif
 
 
@@ -324,7 +322,9 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
